389a70f0ded0e5c64e82f45f23cc151dfcf7dc94
erickcifu <erickalfred29@gmail.com>
2020-12-13 20:53:03 -0600

    realice el modelo de factura y resolucion de errores

--- ProyectoFinal/CarpetaProductos/models.py
+++ ProyectoFinal/CarpetaProductos/models.py
@@ -8,3 +8,32 @@ class empleado(models.Model):
 
     def __str__(self):
         return self.usuarios
+
+class categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+class producto(models.Model):
+    nombre_producto = models.CharField(max_length=30)
+    Descripcion = models.CharField(max_length=300)
+    Existencia = models.IntegerField()
+    Precio = models.FloatField()
+    Categoria = models.ForeignKey(
+    categoria,  on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.nombre_producto
+
+class factura(models.Model):
+    Nombre_Cliente = models.CharField(max_length=100)
+    Producto = models.ForeignKey(producto,related_name="producto", on_delete=models.CASCADE)
+    serie = models.IntegerField()
+    nit = models.CharField(max_length=30)
+    cantidad = models.IntegerField()
+    Fecha = models.DateField()
+    total = models.FloatField()
+
+    def __int__(self):
+        return self.serie
